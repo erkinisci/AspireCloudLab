26a07d3a00aa2f5ee308817fbeef3e6eea56d26a
Erkin işçi <erkin.isci@gmail.com>
2024-02-29 20:26:16 +0100

    feat: add prometheus & grafana

--- AspireCloudLab.AppHost/Program.cs
+++ AspireCloudLab.AppHost/Program.cs
@@ -1,11 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+var cache = builder.AddRedis("redis-aspirational");
+
+var grafana = builder.AddContainer("grafana", "grafana/grafana")
+    .WithVolumeMount("../grafana/config", "/etc/grafana")
+    .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
+    .WithEndpoint(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
+
+builder.AddContainer("prometheus", "prom/prometheus")
+    .WithVolumeMount("../prometheus", "/etc/prometheus")
+    .WithEndpoint(9090, hostPort: 9090);
 
 var apiService = builder.AddProject<Projects.AspireCloudLab_ApiService>("apiservice");
 
 builder.AddProject<Projects.AspireCloudLab_Web>("webfrontend")
     .WithReference(cache)
-    .WithReference(apiService);
+    .WithReference(apiService)
+    .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"));
 
 builder.Build().Run();
