7e5406e2b4203fd5aa7219ca1d468e8d778f80f5
Erkin işçi <erkin.isci@gmail.com>
2024-02-29 20:40:46 +0100

    feat: uncomment scraping of prometheus

--- AspireCloudLab.ServiceDefaults/Extensions.cs
+++ AspireCloudLab.ServiceDefaults/Extensions.cs
@@ -97,7 +97,7 @@ public static class Extensions
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
         // Uncomment the following line to enable the Prometheus endpoint (requires the OpenTelemetry.Exporter.Prometheus.AspNetCore package)
-        // app.MapPrometheusScrapingEndpoint();
+        app.MapPrometheusScrapingEndpoint();
 
         // All health checks must pass for app to be considered ready to accept traffic after starting
         app.MapHealthChecks("/health");
